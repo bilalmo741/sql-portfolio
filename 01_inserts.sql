6527d16ec5f439805fa512bd06603331ea1a1111
bilalmo741 <bilalm74881@gmail.com>
2025-09-01 12:33:29 -0700

    Update 01_inserts.sql

--- 01_inserts.sql
+++ 01_inserts.sql
@@ -1,8 +1,6 @@
--- Insert sample review
+-- Insert sample film reviews
 INSERT INTO FilmReviews (ReviewID, ReviewerName, Rating, Date, FilmID)
 VALUES (6, 'Ryan Reynolds', 4.2, '2011-11-24', 9);
 
--- Update a film duration
-UPDATE Films
-SET Duration = '2hr30m'
-WHERE FilmID = 1;
+INSERT INTO FilmReviews (ReviewID, ReviewerName, Rating, Date, FilmID)
+VALUES (7, 'Lionel Messi', 4.6, '2021-02-09', 18);
