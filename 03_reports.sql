3f9fee31cbe61adba9989f0ea49d68896f3f658a
bilalmo741 <bilalm74881@gmail.com>
2025-09-01 12:33:54 -0700

    Update 03_reports.sql

--- 03_reports.sql
+++ 03_reports.sql
@@ -1,1 +1,10 @@
+-- Average rating across all movies
+SELECT AVG(Rating) AS AvgMovieRating
+FROM Movies;
+
+-- Average rating per movie with HAVING filter
+SELECT Title, AVG(Rating) AS AvgRating
+FROM Movies
+GROUP BY Title
+HAVING AVG(Rating) > 3.5;
 
