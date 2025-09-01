4fc4167e37ee396ce7266e0caefc1880aac2ab86
bilalmo741 <bilalm74881@gmail.com>
2025-09-01 12:33:42 -0700

    Update 02_updates.sql

--- 02_updates.sql
+++ 02_updates.sql
@@ -1,1 +1,10 @@
+-- Update a film duration
+UPDATE Films
+SET Duration = '2hr30m'
+WHERE FilmID = 1;
+
+-- Update TV series rating
+UPDATE TVSeries
+SET Rating = 4.8
+WHERE Title = 'Breaking Bad';
 
